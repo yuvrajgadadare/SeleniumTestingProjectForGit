dfdec7ae41cd5ceb5c704f168f9745b809e298da
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-09-03 19:12:16 +0530

    sgf

---
+++ src/main/java/demopackage/Abc.java
@@ -0,0 +1,15 @@
+package demopackage;
+
+public class Abc {
+
+	void Add() {
+		int a,b,c
+		c=a+b;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
