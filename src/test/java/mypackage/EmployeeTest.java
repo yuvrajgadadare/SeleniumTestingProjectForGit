34e60812cde24a35b8f169f917ac57327f8a9ff4
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2024-09-03 18:58:45 +0530

    Added

---
+++ src/test/java/mypackage/EmployeeTest.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class EmployeeTest {
+
+	public void AddEmployee() {
+		System.out.println("This is AddEmployee Function");
+	}
+}
--- src/test/java/mypackage/MyClass.java
+++ src/test/java/mypackage/MyClass.java
@@ -5,6 +5,9 @@ public class MyClass {
 	public void SampleTest() {
 		System.out.println("Welcome");
 	}
+	public void SecondTest() {
+		System.out.println("This is Second Test");
+	}
 	public static void main(String[] args) {
  
 		MyClass m =new MyClass();
